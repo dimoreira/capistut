b95833d1ee5923ffc141f9dc6bedd40c60341928
Diego Moreira <diegoalvesmoreira@gmail.com>
2013-01-17 00:40:40 -0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capistut::Application.config.secret_token = 'ca61168cb271a2224051714ec66cc0be973c28169aef2f9ca439040c6952a8742eacc9153e7d921b9390d18eb9f86a645d7590f5a07e20e4ce71c3aa394c5e80'
